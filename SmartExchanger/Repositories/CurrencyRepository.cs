09dc7ec7102bc723b024ec1af7e4cdb69a0eca9f
kiviuu <81482742+anonimbz@users.noreply.github.com>
2026-07-07 16:23:48 +0200

    Dodaj pliki projektów.

---
+++ SmartExchanger/Repositories/CurrencyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartExchanger.Repositories
+{
+    internal class CurrencyRepository
+    {
+        public CurrencyRepository()
+        {
+            
+        }
+    }
+}
